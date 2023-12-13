2fbafd1300682c77c56d2ee9e045228991512734
hodori0719 <hodori2003@gmail.com>
2023-12-13 14:35:26 -0500

    script

--- run_ml100k.sh
+++ run_ml100k.sh
@@ -6,6 +6,9 @@
 #SBATCH --mail-type=ALL
 #SBATCH --gpus
 
+module load CUDA
+module load cuDNN
+source activate igmc
 for i in $(seq 1 3)  # to run with different seeds
 do
   python3 Main.py --data-name ml_100k --save-appendix _mnph200 --data-appendix _mnph200 --epochs 40  --max-nodes-per-hop 200 --seed ${i} --testing --ensemble --dynamic-train
